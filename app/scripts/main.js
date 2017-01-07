22f88b39349db254a6774f03950423f9aac90406
Nicolás Silva <sovietfrance@gmail.com>
2017-01-06 21:04:25 -0300

    fix(): Display only last five responses, according to the requirement

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,3 @@
-
 /* Requirement:
 
             arr[0] // ignore
@@ -9,59 +8,65 @@ ASK_SIZE  = arr[4]; // float Size of the last lowest ask
             arr[5] // ignore
 
 1. - Qué tamaño va teniendo la diferencia entre el precio más alto
-    al que compran y el más bajo al que venden. (Cómo se comparan los BID y los ASK.)
-    (BID - ASK)
+     al que compran y el más bajo al que venden. (Cómo se comparan los BID y los ASK.)
+     (BID - ASK)
 
 2. - Qué tamaño va teniendo la diferencia entre la cantidad que están
-    dispuestos a comprar y la cantidad que están dispuestos a vender
-    (BID_SIZE - ASK_SIZE)
+     dispuestos a comprar y la cantidad que están dispuestos a vender
+     (BID_SIZE - ASK_SIZE)
 
 - Cuál es el precio de cada BID y cada ASK */
-
 
 var message = {
     event: 'subscribe',
     channel: 'ticker',
     symbol: 'tBTCUSD'
 };
-
+var countResponses = 0;
 var websocket = new WebSocket('wss://api.bitfinex.com/ws/v2');
 
 websocket.onopen = function() {
-    $('#response').append('<p>> CONNECTED</p>');
+    $('#init').append('<p>> CONNECTED</p>');
 };
 
 websocket.onmessage = function(evt) {
     var responseArray = JSON.parse(evt.data)
-    if(responseArray.length >= 5) {
-      parseData( responseArray )
+    if (responseArray.length >= 5) {
+        parseData(responseArray)
     }
 };
 
 websocket.onerror = function(evt) {
-    $('#response').append('<p>> ERROR: ' + evt.data + '</p>');
+    $('#init').append('<p>> ERROR: ' + evt.data + '</p>');
 };
 
 function sendMessage(message) {
-    $('#response').append('<p>> SENT: ' + JSON.stringify(message) + '</p>');
+    $('#init').append('<p>> SENT: ' + JSON.stringify(message) + '</p>');
     websocket.send(JSON.stringify(message));
+};
+
+function parseData(data) {
+    var BID = data[1];
+    var BID_SIZE = data[2];
+    var ASK = data[3];
+    var ASK_SIZE = data[4];
+
+    var template = '';
+    template += '<div class="item"><hr>';
+    template += '<p>> <b>BID:</b> ' + BID + ' <b>ASK:</b> ' + ASK + '</p>';
+    template += '<p>> (BID - ASK): ' + (BID - ASK) + '</p>';
+    template += '<p>> (BID_SIZE - ASK_SIZE): ' + (BID_SIZE - ASK_SIZE) + '</p>';
+    template += '<hr></div>';
+
+    // Display only last 5
+    if (countResponses == 5) {
+        countResponses = 4;
+        $('#responses').find('div').last().remove();
+    }
+    countResponses++;
+    $('#responses').prepend(template);
 };
 
 setTimeout(function() {
     sendMessage(message)
-}, 2000)
-
-function parseData(data){
-
-  var BID      = data[1];
-  var BID_SIZE = data[2];
-  var ASK      = data[3];
-  var ASK_SIZE = data[4];
-
-  $('#response').append('<hr>');
-  $('#response').append('<p>> <b>BID:</b> ' + BID + ' <b>ASK:</b> ' + ASK + '</p>');
-  $('#response').append('<p>> (BID - ASK): ' + (BID - ASK) + '</p>');
-  $('#response').append('<p>> (BID_SIZE - ASK_SIZE): ' + (BID_SIZE - ASK_SIZE) + '</p>');
-  $('#response').append('<hr>');
-}
-
+}, 2000);
