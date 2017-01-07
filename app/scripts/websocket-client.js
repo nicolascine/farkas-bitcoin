0575cc50051a85b7768d136fac38611a77033227
Nicolás Silva <sovietfrance@gmail.com>
2017-01-07 05:32:24 -0300

    refactor(JS): Update WebsocketClient class, remove parseData method, save stream result on array

--- app/scripts/websocket-client.js
+++ app/scripts/websocket-client.js
@@ -1,54 +1,34 @@
 class WebsocketClient {
     constructor() {
-        this.websocket = new WebSocket('wss://api.bitfinex.com/ws/v2');
-        this.websocket.onopen = () => { this.socketOpen() };
-        this.websocket.onmessage = (evt) => { this.socketMessage(evt) };
-        this.websocket.onerror = (evt) => { this.socketError(evt) };
-        this.countResponses = 0;
+        this.websocket = new WebSocket('wss://api.bitfinex.com/ws/v2')
+        this.websocket.onopen = () => {
+            this.socketOpen()
+        }
+        this.websocket.onmessage = (evt) => {
+            this.socketMessage(evt)
+        }
+        this.websocket.onerror = (evt) => {
+            this.socketError(evt)
+        }
+        this.streamArray = []
     }
-
     socketOpen() {
-        $('#init').append('<p>> CONNECTED</p>');
+        $('#init').append('<p>> CONNECTED</p>')
     }
-
     socketMessage(evt) {
-        var responseArray = JSON.parse(evt.data)
-        if (responseArray.length >= 5) {
-            this.parseData(responseArray)
+        if (this.responseIsValid(evt.data)) {
+            this.streamArray.push(evt.data)
         }
     }
-
     socketError(evt) {
-        $('#init').append('<p>> ERROR: ' + evt.data + '</p>');
+        $('#init').append('<p>> ERROR: ' + evt.data + '</p>')
     }
-
     sendMessage(message) {
-        console.log(message);
-        $('#init').append('<p>> SENT: ' + JSON.stringify(message) + '</p>');
-        this.websocket.send(JSON.stringify(message));
-    };
-
-    parseData(data) {
-        var BID = data[1];
-        var BID_SIZE = data[2];
-        var ASK = data[3];
-        var ASK_SIZE = data[4];
-        var date = moment().format('DD-MM-YYYY HH:MM:SS');
-
-        var template = '<li class="item">\
-                        <span class="date"><i class="material-icons">keyboard_arrow_right</i>' + date + '</span><br>\
-                        <b class="green">BID:</b> ' + BID + ' <b class="red">ASK:</b> ' + ASK + '<br>\
-                        <span class="green">BID - ASK:</span> ' + (BID - ASK) + '<br>\
-                        <span class="red">BID_SIZE - ASK_SIZE:</span> ' + (BID_SIZE - ASK_SIZE) + '\
-                        </li>';
-
-        // Display only last 5
-        if (this.countResponses == 5) {
-            this.countResponses = 4;
-            $('#responses').find('li').last().remove();
-        }
-        this.countResponses++;
-        $('#responses').prepend(template);
-    };
-
+        $('#init').append('<p>> SENT: ' + JSON.stringify(message) + '</p>')
+        this.websocket.send(JSON.stringify(message))
+    }
+    responseIsValid(res) {
+        var isValid = JSON.parse(res).length >= 5 ? true : false;
+        return isValid
+    }
 }
