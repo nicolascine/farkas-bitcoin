072793d4228891b21ae087d87a7c075dac278fb5
Nicolás Silva <sovietfrance@gmail.com>
2017-01-07 23:19:56 -0300

    feat(chart): Add new BitfinexChart Class

---
+++ app/scripts/bitfinex-chart.js
@@ -0,0 +1,9 @@
+class BitfinexChart {
+    constructor(container) {
+        this.container = container
+        this.displayChart()
+    }
+    displayChart() {
+        this.container.html('>>chart here')
+    }
+}
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,16 +5,27 @@ const message = {
     symbol: 'tBTCUSD'
 }
 const sidebarContainer = $('#responses')
+const chartContainer = $('#farkas-plugin')
+
+class FarkasChart extends BitfinexChart {
+    constructor(chartContainer) {
+        super(chartContainer)
+    }
+}
+
 class FarkasSidebar extends SidebarPlugin {
     constructor(sidebarContainer) {
         super(sidebarContainer)
     }
 }
+
 class BitfinexWebSocket extends WebsocketClient {
     constructor(endpoint, message) {
         super(endpoint, message)
     }
 }
+
+const chart = new FarkasChart(chartContainer);
 const sidebar = new FarkasSidebar(sidebarContainer);
 const bitfinex = new BitfinexWebSocket(endpoint, message);
 
